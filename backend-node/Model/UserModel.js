af2c05734a69e04bdc17c992280e8c86b134f647
AnojPeiris <it21161056@my.sliit.lk>
2024-09-10 21:09:47 +0530

    refactor: refactor backend code base

--- backend-node/Model/UserModel.js
+++ backend-node/Model/UserModel.js
@@ -1,40 +1,39 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-    lastName:{
-        type:String,
-        require:true
-    },
-    initials:{
-        type:String,
-        require:true
-    },
-    email:{
-        type:String,
-        require:true
-    },
-    mobileNumber:{
-        type:String,
-        require:true
-    },
-    faculty:{
-        type:String,
-        require:true
-    },
-    regNumber:{
-        type:String,
-        require:true
-    },
-    password:{
-        type:String,
-        require:true
-    },
-    role:{
-        type:String,
-        require:true
-    }
-
-
-})
+  lastName: {
+    type: String,
+    require: true,
+  },
+  initials: {
+    type: String,
+    require: true,
+  },
+  email: {
+    type: String,
+    require: true,
+  },
+  mobileNumber: {
+    type: String,
+    require: true,
+  },
+  faculty: {
+    type: String,
+    require: true,
+  },
+  regNumber: {
+    type: String,
+    require: true,
+  },
+  password: {
+    type: String,
+    require: true,
+  },
+  role: {
+    type: String,
+    require: true,
+    enum: ["student", "user", "lecturer", "admin"], // Fixed roles
+  },
+});
 module.exports = mongoose.model("User", UserSchema);
