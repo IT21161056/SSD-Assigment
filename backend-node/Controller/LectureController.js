8ed79ad9b88b5f8b983606470e3a51cbe009bb4f
Harini Senevirathne <99156018+IT21386022@users.noreply.github.com>
2024-09-26 23:20:32 +0530

    added https status code

--- backend-node/Controller/LectureController.js
+++ backend-node/Controller/LectureController.js
@@ -1,3 +1,141 @@
+// const LectureModel = require("../Model/LectureModel");
+// const { CustomError } = require("../exceptions/baseException");
+// const { tryCatch } = require("../utils/tryCatchWrapper");
+// const path = require("path");
+// const cloudinary = require("../utils/cloudinary");
+
+// const fileInArray = [];
+
+// //getAll
+// const getAllLecture = tryCatch(async (req, res, next) => {
+//   const lectures = await LectureModel.find();
+
+//   if (!lectures) throw new CustomError("Not found");
+
+//   return res.status(200).json(lectures);
+// });
+
+// //add
+// const addLecture = tryCatch(async (req, res) => {
+//   console.log(req.files);
+
+//   const {
+//     year,
+//     semester,
+//     topic,
+//     subject,
+//     date,
+//     time,
+//     discription,
+//     meeting_link,
+//   } = req.body;
+
+//   let pdfData = null;
+
+//   if (req.files && req.files.length > 0) {
+//     for (let file of req.files) {
+//       let fileExtension = path.extname(file.originalname).toLowerCase();
+//       if (fileExtension === ".pdf") {
+//         pdfData = await cloudinary.uploader.upload(file.path, {
+//           pages: true,
+//         });
+//       }
+//     }
+//   }
+
+//   const newLecture = new LectureModel({
+//     year,
+//     semester,
+//     topic,
+//     subject,
+//     date,
+//     time,
+//     discription: discription,
+//     meeting_link,
+//     pdf: pdfData ? pdfData.secure_url : null,
+//     cloudinary_id_pdf: pdfData ? pdf.public_id : null,
+//   });
+
+//   const response = await newLecture.save();
+
+//   res.status(201).json(response);
+// });
+
+// //getByid
+// const getLectureById = tryCatch(async (req, res, next) => {
+//   const id = req.params.id;
+//   const lecture = await LectureModel.findById(id);
+
+//   if (!lecture) throw new CustomError("Not found");
+
+//   return res.status(200).json(lecture);
+// });
+
+// //delete
+// const deleteLecture = tryCatch(async (req, res, next) => {
+//   const id = req.params.id;
+//   const lecture = await LectureModel.findByIdAndRemove(id);
+
+//   if (!lecture) throw new CustomError("cannot delete");
+
+//   return res.status(200).json({ message: `Lecture ${id} deleted` });
+// });
+
+// // update
+// const updateLecture = tryCatch(async (req, res) => {
+//   const id = req.params.id;
+
+//   const {
+//     year,
+//     semester,
+//     topic,
+//     subject,
+//     date,
+//     time,
+//     discription,
+//     meeting_link,
+//   } = req.body;
+
+//   let pdfData = null;
+
+//   if (req.files && equal.files.length > 0) {
+//     for (let file of req.files) {
+//       let fileExtension = path.extname(file.originalname).toLowerCase();
+//       if (fileExtension === ".pdf") {
+//         pdfData = await cloudinary.uploader.upload(file.path, { pages: true });
+//       }
+//     }
+//   }
+
+//   let updatedLecture = await LectureModel.findByIdAndUpdate(
+//     id,
+//     {
+//       year,
+//       semester,
+//       topic,
+//       subject,
+//       date,
+//       time,
+//       discription: discription,
+//       meeting_link,
+//       pdf: pdfData ? pdfData.secure_url : undefined,
+//       cloudinary_id_pdf: pdfData ? pdfData.public_id : undefined,
+//     },
+//     { new: true }
+//   );
+
+//   if (!updatedLecture) throw new CustomError("Not found");
+//   res.json(updatedLecture);
+// });
+
+// module.exports = {
+//   addLecture,
+//   deleteLecture,
+//   getLectureById,
+//   getAllLecture,
+//   updateLecture,
+// };
+
 const LectureModel = require("../Model/LectureModel");
 const { CustomError } = require("../exceptions/baseException");
 const { tryCatch } = require("../utils/tryCatchWrapper");
@@ -6,16 +144,16 @@ const cloudinary = require("../utils/cloudinary");
 
 const fileInArray = [];
 
-//getAll
+// Get all lectures
 const getAllLecture = tryCatch(async (req, res, next) => {
   const lectures = await LectureModel.find();
 
-  if (!lectures) throw new CustomError("Not found");
+  if (!lectures || lectures.length === 0) throw new CustomError("Not found", 404);
 
   return res.status(200).json(lectures);
 });
 
-//add
+// Add new lecture
 const addLecture = tryCatch(async (req, res) => {
   console.log(req.files);
 
@@ -53,35 +191,35 @@ const addLecture = tryCatch(async (req, res) => {
     discription: discription,
     meeting_link,
     pdf: pdfData ? pdfData.secure_url : null,
-    cloudinary_id_pdf: pdfData ? pdf.public_id : null,
+    cloudinary_id_pdf: pdfData ? pdfData.public_id : null,
   });
 
   const response = await newLecture.save();
 
-  res.status(201).json(response);
+  return res.status(201).json(response);
 });
 
-//getByid
+// Get lecture by ID
 const getLectureById = tryCatch(async (req, res, next) => {
   const id = req.params.id;
   const lecture = await LectureModel.findById(id);
 
-  if (!lecture) throw new CustomError("Not found");
+  if (!lecture) throw new CustomError("Lecture not found", 404);
 
   return res.status(200).json(lecture);
 });
 
-//delete
+// Delete lecture by ID
 const deleteLecture = tryCatch(async (req, res, next) => {
   const id = req.params.id;
   const lecture = await LectureModel.findByIdAndRemove(id);
 
-  if (!lecture) throw new CustomError("cannot delete");
+  if (!lecture) throw new CustomError("Lecture not found", 404);
 
   return res.status(200).json({ message: `Lecture ${id} deleted` });
 });
 
-// update
+// Update lecture by ID
 const updateLecture = tryCatch(async (req, res) => {
   const id = req.params.id;
 
@@ -98,7 +236,7 @@ const updateLecture = tryCatch(async (req, res) => {
 
   let pdfData = null;
 
-  if (req.files && equal.files.length > 0) {
+  if (req.files && req.files.length > 0) {
     for (let file of req.files) {
       let fileExtension = path.extname(file.originalname).toLowerCase();
       if (fileExtension === ".pdf") {
@@ -124,8 +262,9 @@ const updateLecture = tryCatch(async (req, res) => {
     { new: true }
   );
 
-  if (!updatedLecture) throw new CustomError("Not found");
-  res.json(updatedLecture);
+  if (!updatedLecture) throw new CustomError("Lecture not found", 404);
+
+  return res.status(200).json(updatedLecture);
 });
 
 module.exports = {
--- backend-node/Controller/LibraryItemController.js
+++ backend-node/Controller/LibraryItemController.js
@@ -1,3 +1,105 @@
+// const cloudinary = require("../utils/cloudinary");
+// const multer = require("multer");
+// const uuid = require("uuid").v4;
+// const path = require("path");
+// const PDF = require("../Model/LibraryItemModel");
+// const { CustomError } = require("../exceptions/baseException");
+// const { tryCatch } = require("../utils/tryCatchWrapper");
+
+// const storage = multer.diskStorage({
+//   destination: (req, file, cb) => {
+//     cb(null, "uploads");
+//   },
+//   filename: (req, file, cb) => {
+//     const ext = path.extname(file.originalname);
+//     const id = uuid();
+//     const filePath = `${id}${ext}`;
+//     cb(null, filePath);
+//   },
+// });
+
+// const upload = multer({
+//   fileFilter: (req, file, cb) => {
+//     if (
+//       file.mimetype === "image/png" ||
+//       file.mimetype === "image/jpg" ||
+//       file.mimetype === "image/jpeg" ||
+//       file.mimetype === "application/pdf"
+//     ) {
+//       cb(null, true);
+//     } else {
+//       cb(null, false);
+//       return cb(new Error("Only .png, .jpg, .jpeg and .pdf format allowed!"));
+//     }
+//   },
+//   storage: storage,
+// });
+
+// const addItem = tryCatch(async (req, res) => {
+//   upload.array("uploaded_Image", 10)(req, res, async (err) => {
+//     if (err instanceof multer.MulterError) {
+//       return res.status(400).json({ error: err.message });
+//     } else if (err) {
+//       return res.status(500).json({ error: "File upload error" });
+//     }
+
+//     const fileInArray = [];
+//     let pdff = null;
+
+//     if (req.files && req.files.length > 0) {
+//       for (let file of req.files) {
+//         const ext = path.extname(file.originalname).slice(1); // e.g., 'pdf'
+//         const filePath = file.filename;
+//         fileInArray.push(filePath);
+
+//         if (ext === "pdf") {
+//           pdff = await cloudinary.uploader.upload(
+//             `${path.resolve(__dirname, "../uploads")}/${filePath}`,
+//             { pages: true }
+//           );
+//         }
+//       }
+//     }
+
+//     const item = new PDF({
+//       faculty: req.body.faculty,
+//       year: req.body.year,
+//       subject: req.body.subject,
+//       pdf: pdff ? pdff.secure_url : null,
+//       cloudinary_id_pdf: pdff ? pdff.public_id : null,
+//       other_files: fileInArray,
+//     });
+
+//     const response = await item.save();
+//     return res.status(201).json(response);
+//   });
+// });
+
+// const getAllItem = tryCatch(async (req, res) => {
+//   const pdfs = await PDF.find();
+//   res.json(pdfs);
+// });
+
+// const getItemById = tryCatch(async (req, res) => {
+//   const id = req.params.id;
+//   const pdf = await PDF.findById(id);
+
+//   if (!pdf) throw new CustomError("Resource not found!", 404);
+
+//   res.status(200).json({ pdf });
+// });
+
+// const deleteItem = tryCatch(async (req, res) => {
+//   const id = req.params.id;
+//   const pdf = await PDF.findByIdAndRemove(id);
+
+//   if (!pdf) throw new CustomError("Resource not found!", 404);
+
+//   res.status(200).json({ message: `Item ${id} deleted successfully` });
+// });
+
+// module.exports = { addItem, getAllItem, deleteItem, getItemById };
+
 const cloudinary = require("../utils/cloudinary");
 const multer = require("multer");
 const uuid = require("uuid").v4;
@@ -38,9 +140,9 @@ const upload = multer({
 const addItem = tryCatch(async (req, res) => {
   upload.array("uploaded_Image", 10)(req, res, async (err) => {
     if (err instanceof multer.MulterError) {
-      return res.status(400).json({ error: err.message });
+      return res.status(400).json({ error: err.message }); // 400 Bad Request (Multer error)
     } else if (err) {
-      return res.status(500).json({ error: "File upload error" });
+      return res.status(500).json({ error: "File upload error" }); // 500 Internal Server Error (Other error)
     }
 
     const fileInArray = [];
@@ -71,31 +173,35 @@ const addItem = tryCatch(async (req, res) => {
     });
 
     const response = await item.save();
-    return res.status(201).json(response);
+    return res.status(201).json(response); // 201 Created
   });
 });
 
+// Get all items
 const getAllItem = tryCatch(async (req, res) => {
   const pdfs = await PDF.find();
-  res.json(pdfs);
+  if (!pdfs || pdfs.length === 0) throw new CustomError("No items found!", 404); // 404 Not Found (No items)
+  return res.status(200).json(pdfs); // 200 OK
 });
 
+// Get item by ID
 const getItemById = tryCatch(async (req, res) => {
   const id = req.params.id;
   const pdf = await PDF.findById(id);
 
-  if (!pdf) throw new CustomError("Resource not found!", 404);
+  if (!pdf) throw new CustomError("Resource not found!", 404); // 404 Not Found (Invalid ID)
 
-  res.status(200).json({ pdf });
+  return res.status(200).json({ pdf }); // 200 OK
 });
 
+// Delete item by ID
 const deleteItem = tryCatch(async (req, res) => {
   const id = req.params.id;
   const pdf = await PDF.findByIdAndRemove(id);
 
-  if (!pdf) throw new CustomError("Resource not found!", 404);
+  if (!pdf) throw new CustomError("Resource not found!", 404); // 404 Not Found (Invalid ID)
 
-  res.status(200).json({ message: `Item ${id} deleted successfully` });
+  return res.status(200).json({ message: `Item ${id} deleted successfully` }); // 200 OK (Deleted)
 });
 
 module.exports = { addItem, getAllItem, deleteItem, getItemById };
